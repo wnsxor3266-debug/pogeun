15196821e59f019300eabcede2cc77d83a44057c
USER <selpickent@gmail.com>
2026-08-03 11:24:50 +0900

    Add product images and coupang links

--- src/products.ts
+++ src/products.ts
@@ -51,16 +51,18 @@ export const PRODUCTS: Product[] = [
     name: '백색소음기',
     description: '작은 소리에도 잘 깨는 분들에게 도움을 줄 수 있는 백색소음기예요.',
     category: 'sleepGoods',
-    imageUrl: '',
-    affiliateUrl: '', // TODO: 쿠팡파트너스 링크
+    imageUrl:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/7918/549ac4cefdef7ade607cf1ee1cd0cfb17b31d9eeac60d29b2372d1e969ab.jpg',
+    affiliateUrl: 'https://link.coupang.com/a/fT8qnO2m4q',
   },
   {
     id: 'blue-light-glasses',
     name: '블루라이트 차단 안경',
     description: '자기 전 화면을 자주 보는 분들이 많이 찾는 아이템이에요.',
     category: 'sleepGoods',
-    imageUrl: '',
-    affiliateUrl: '', // TODO: 쿠팡파트너스 링크
+    imageUrl:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/ecc6/b1efd892b4df9994489eb782fd08e63b4ddd50d5966d36a67d71a1c7f2fa.png',
+    affiliateUrl: 'https://link.coupang.com/a/fT8mEALJBY',
   },
   {
     id: 'aroma-diffuser',
@@ -76,15 +78,26 @@ export const PRODUCTS: Product[] = [
     name: '수면 유도 차 (캐모마일 등)',
     description: '따뜻한 차 한 잔으로 몸을 이완하고 싶은 분들이 찾는 아이템이에요.',
     category: 'aroma',
-    imageUrl: '',
-    affiliateUrl: '', // TODO: 쿠팡파트너스 링크
+    imageUrl:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/image_audit/stage/manual/643079d33eb440ca256db0de000b097ec825439b3eee1c809e05e3a55535_1751876473731.jpg',
+    affiliateUrl: 'https://link.coupang.com/a/fT8dxCIL9w',
   },
   {
     id: 'memory-foam-pillow',
     name: '메모리폼 베개',
     description: '목과 어깨가 자주 뻐근한 분들이 많이 찾는 베개예요.',
     category: 'bedding',
-    imageUrl: '',
-    affiliateUrl: '', // TODO: 쿠팡파트너스 링크
+    imageUrl:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/600e/a9114d6e1ea823e9d42ec96d07a6578e9fa7fd6f622a8215ac5ff4c8d319.jpg',
+    affiliateUrl: 'https://link.coupang.com/a/fT8jjJIMYm',
+  },
+  {
+    id: 'weighted-blanket',
+    name: '중력 이불',
+    description: '몸을 부드럽게 눌러주는 무게감으로 편안한 잠자리를 도와주는 담요예요.',
+    category: 'bedding',
+    imageUrl:
+      'https://thumbnail.coupangcdn.com/thumbnails/remote/492x492ex/image/vendor_inventory/346e/5e5955a23ad4e2041c044570433ec5b5ac05cf109cb61794090e3ae4080b.png',
+    affiliateUrl: 'https://link.coupang.com/a/fT79argMx2',
   },
 ]
